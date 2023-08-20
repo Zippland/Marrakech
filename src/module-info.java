b545bf47f9c6b476dd196e008846c1ef6ca856ee
Zihan Jian <u7174903@anu.edu.au>
2023-08-20 21:52:34 +1000

    test

---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+module comp1110.ass2 {
+    requires javafx.fxml;
+    requires  javafx.controls;
+
+    exports comp1110.ass2.gui;
+
+}
+// --module-path ${PATH_TO_FX} --add-modules javafx.controls,javafx.fxml
